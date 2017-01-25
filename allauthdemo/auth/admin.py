b44de8cd1b090534d89a7a089b09608a6550dd12
nagat57 <nagat.mktar57@gmail.com>
2017-01-25 16:55:23 +0000

    Update admin.py

--- allauthdemo/auth/admin.py
+++ allauthdemo/auth/admin.py
@@ -1,87 +1,19 @@
-from django.contrib import admin
-#from django.utils.html import format_html_join
-#from django.utils.safestring import mark_safe
-#from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.admin import UserAdmin
-try:
-    from django.utils.encoding import force_text
-except ImportError:
-    from django.utils.encoding import force_unicode as force_text
 
-from .models import DemoUser, UserProfile
-from .forms import DemoUserAdminForm
+Assalamu alaikum,  Praise be to Allah, The Most beneficence, The most merciful May Allah Swt always gives rahmat and barakah to the lord,lord families and countries, Amin. 
 
 
-class UserProfileAdmin(admin.ModelAdmin):
-    search_fields = ('user', 'dob')
-    ordering = ('user',)
-    list_select_related = ('user',)
+I pray that this mail finds you in a good mood. I'm Mrs. Nagat Mohamed Mukhtar, from Libya. But currently on political asylum here in Algeria with my two daughters. My husband Mr. El Mohamed Mukhtar was a personal adviser to the interim Prime Minister Mahmoud Jibril,under the tenure of President Muammar Muhammad Abu Minyar Al-Gaddafi who was brutally murdered by the rebels, along with many recognized personalities that possesses high position in the country including my husband.
 
+The crisis prolonged which result our evacuation from our country Libya down to this place in Algeria. My late husband before was also an Oil Mogul in our country, but we lost everything in the recent crisis, because the rebel government that took over power in Libya, killed highly positioned men and woman, and confiscated their properties and frozen their accounts, after they have succeeded in killing President Muammar Muhammad Abu Minyar Al-Gaddafi.
 
-admin.site.register(UserProfile, UserProfileAdmin)
+However, I'm contacting you because I believe that you will be of great assistance to me. 
+Before the death of my husband, he made a deposit of $8,400,000.00 (Eight Million Four Hundred Thousand United States dollars) in a private Security Firm in Ivory Coast (West Africa) the money was purposed to be invested in Cocoa business in Ivory Coast before his unprepared death.
 
+The fund in question was concealed in a metallic trunk box and was deposited under the care of a Private Security Firm for safe keeping, but he declared the content of the box to be our FAMILY VALUABLES, which means that the management does not know the real content of the box; he purposely did that, for security reason. Therefore, it is confidential, I hope you understand?
+My main purpose of contacting you is to seek your help, as I want you to help me and secured the box from the company, and arrange with the company, so that they will bring the box over to you in your country. 
 
-class UserProfileAdminInline(admin.TabularInline):
-    model = UserProfile
+Please reply as urgent as possible if you agree, so that I can email the details to you.
 
-
-class DemoUserAdmin(UserAdmin):
-    """The project uses a custom User model, so it uses a custom User admin model.
-
-    Some related notes at:
-    https://github.com/dabapps/django-email-as-username/blob/master/emailusernames/admin.py
-
-    And:
-    .../lib/python2.7/site-packages/django/contrib/auth/admin.py
-    """
-
-    inlines = [
-        UserProfileAdminInline,
-        ]
-
-    #readonly_fields = ('private_uuid', 'public_id')
-
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'display_name')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-        #(_('Ids'), {'fields': ('private_uuid', 'public_id')}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')}
-        ),
-    )
-    list_display = ('email', 'first_name', 'last_name', 'display_name', 'is_staff')
-    search_fields = ('first_name', 'last_name', 'display_name', 'email')
-    ordering = ('email',)
-
-    form = DemoUserAdminForm
-
-
-
-
-# *** NOTE ***
-# As the site uses email instead of username, I'm changing how a User object
-# displays or identifies itself in admin. The default in Django (file is
-# lib/python2.7/site-packages/django/contrib/auth/models.py) is
-#
-#    def __str__(self):
-#        return self.get_username()
-#
-#    def natural_key(self):
-#        return (self.get_username(),)
-#
-# I'm overriding that a cheap way. I'm not sure if I should replace the entire
-# User object ... might be better.
-#
-#User.__unicode__ = lambda(u): u.email
-#User.natural_key = lambda(u): (u.email,)
-
-#admin.site.unregister(DjangoDefaultUser)
-admin.site.register(DemoUser, DemoUserAdmin)
+Kind regards,
+Mrs Mukhtar.
+ Email: mnagat794@yahoo.com
